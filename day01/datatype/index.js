10c935e0787f1fea62de49772812107f11ef91c3
paili2 <zxcv7502@naver.com>
2025-02-20 21:43:43 +0900

    Add files via upload

---
+++ day01/datatype/index.js
@@ -0,0 +1,26 @@
+//  데이터 타입 종류
+
+// 문자 타입: "", ``, ''
+//  숫자 타입: 아라비아 숫자
+
+// const name = "정서령";
+// const age = 30;
+// const birth = 2000;
+// const coffee = "아메리카노";
+
+// 더하기에 문자 타입이 들어가면 모두 문자 취급함
+
+// window.console.log(age + birth); // 2030
+// window.console.log(coffee + age); // 아메리카노30
+// window.console.log(age + coffee + birth);
+
+// const age1 = "17";
+// const age2 = "19";
+// window.console.log(age1 + age2);
+
+// prompt는 문자취급
+const age = window.prompt("숫자 입력");
+
+const number_age = Number(age);
+
+window.alert(`${number_age}의 10을 더한 값은: ${number_age + 10}`);
---
+++ day01/variable/index.js
@@ -0,0 +1,10 @@
+//  variable (변수)
+const ice = 1 + 9;
+const fire = 10 - 2;
+
+window.console.log(ice); // 10
+window.console.log(fire); //8
+
+const place = "한강 마포";
+//  ` (백틱)
+window.console.log(`제가 사는 동네는 ${place}입니다.`);
