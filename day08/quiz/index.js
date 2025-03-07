56a705fd48324139ba0e09dc5857abb041a589b2
paili2 <zxcv7502@naver.com>
2025-03-07 18:24:11 +0900

    Add files via upload

--- day08/quiz/index.js
+++ day08/quiz/index.js
@@ -2,6 +2,9 @@
 // myString에서 알파벳 "a"가 등장하면 전부 "A"로 변환하고,
 // "A"가 아닌 모든 대문자 알파벳은 소문자 알파벳으로 변환하여 return 하는 solution 함수 완성
 
+// 예) myString = "abstract algebra" => "AbstrAct AlgebrA"
+//     myString = "PrOgRaMmErS" => "progrAmmers"
+
 const solution = (myString) =>
   [...myString]
     .map((x) => (x == "a" ? x.toUpperCase() : x.toLowerCase()))
